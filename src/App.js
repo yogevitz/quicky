71e7bc6d4781026c441c6f7e2fe8be591a7dc75f
Yogev Shlomovitz <yogevshlomovitz@gmail.com>
2020-09-26 22:38:06 +0300

    feature: add questions json

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@material-ui/core';
+import questions from './questions.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const keys = Object.keys(questions);
+
+class App extends React.Component {
+  state = {};
+
+  nextQuestion = () => {
+    const rndIndex = Math.floor(Math.random() * keys.length);
+    const question = questions[rndIndex];
+    this.setState({ question });
+  };
+
+  render() {
+    const { question } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div dir="rtl">
+            <p>{question}</p>
+          </div>
+          <Button onClick={() => this.nextQuestion()} variant="contained" color="primary">
+            Next!
+          </Button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
