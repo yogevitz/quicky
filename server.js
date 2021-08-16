16720e72ba93350dcc321647e3e4abeb598d0545
Yogev Shlomovitz <yogevshlomovitz@gmail.com>
2021-08-16 10:54:18 +0300

    fix: set server file as js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// @ts-ignore
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
@@ -8,10 +7,10 @@ app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req: any, res: any) {
+app.get('/ping', function (req, res) {
   return res.send('pong');
 });
-app.get('/*', function (req: any, res: any) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port);
